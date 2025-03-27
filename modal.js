89f00908fef7d863c6505f773f2eff362ce3cc51
kasie1337 <92716111+kasie1337@users.noreply.github.com>
2025-03-27 15:03:49 +0100

    Initial commit

---
+++ modal.js
@@ -0,0 +1,34 @@
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const closemod = document.querySelectorAll(".close");
+
+// launch modal event
+modalBtn.forEach((btn) => {
+  btn.addEventListener("click", launchModal)
+});
+
+closemod.forEach((input) => {
+  input.addEventListener("click", closeModal)
+});
+
+
+
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
+
+function closeModal(){
+  modalbg.style.display = "none";
+}
